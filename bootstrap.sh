86c73fa4c9440eb7a5da749e92ed135cccb7b317
Derek <physci@gmail.com>
2020-02-23 16:00:48 -0500

    adds certificates-ca as package, so rustup installer works

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,21 @@ UHOME=/home/$U
 hostname 6081
 
 apt-get update
-apt-get install -y git build-essential gdb-multiarch qemu-system-misc gcc-riscv64-linux-gnu binutils-riscv64-linux-gnu 
-apt-get install -y libsdl1.2-dev libtool libglib2.0-dev libz-dev libpixman-1-dev gdb pkg-config
+apt-get install -y \
+        git \
+        build-essential \
+        gdb-multiarch \
+        qemu-system-misc \
+        gcc-riscv64-linux-gnu \
+        binutils-riscv64-linux-gnu \
+        ca-certificates \
+        libsdl1.2-dev \
+        libtool \
+        libglib2.0-dev \
+        libz-dev \
+        libpixman-1-dev \
+        gdb \
+        pkg-config
 
 cd $UHOME
 git clone git://github.com/mit-pdos/xv6-riscv-fall19.git
